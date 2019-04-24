85b9a7891ace5103f67d27c0ab4d48a418ba48d8
John Dimm <johndimm@johns-air.attlocal.net>
2019-04-23 23:54:54 -0700

    site-specific

--- app/mysql_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Installation:  supply the missing credentials and save this file as mysql_connect.php
-    $mysql_host = "localhost";
-    $mysql_user = "dishes";
-    $mysql_password = "dishes";
-    $mysql_database = "breakdown";
-?>
