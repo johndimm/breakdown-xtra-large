d68550734cd66ce4d9cccf259414a9caf49d7ba3
johndimm <jdimm@yahoo.com>
2019-04-23 23:44:15 -0700

    Before uploading fix for source switching.

--- app/mysql_connect.php
+++ app/mysql_connect.php
@@ -1,7 +1,7 @@
 <?php
     // Installation:  supply the missing credentials and save this file as mysql_connect.php
-    $mysql_host = "127.0.0.1";
-    $mysql_user = "jdimm";
-    $mysql_password = "Leucate1";
+    $mysql_host = "localhost";
+    $mysql_user = "dishes";
+    $mysql_password = "dishes";
     $mysql_database = "breakdown";
 ?>
