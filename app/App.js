b80a646972cf01d00a89dbe6cb5fb252274e088a
John Dimm <johndimm@johns-air.attlocal.net>
2019-04-24 20:56:35 -0700

    Backticks back.

--- app/App.js
+++ app/App.js
@@ -200,8 +200,7 @@ class App extends React.Component {
     //
     var filter_array = [];
     Object.getOwnPropertyNames(this.state.report.filters).forEach(function (row, i) {
-      //        filter_array.push("`" + row + "` = '" + this.state.report.filters[row].replace("'","''") + "'")
-      filter_array.push(row + " = '" + this.state.report.filters[row].replace("'", "''") + "'");
+      filter_array.push("`" + row + "` = '" + this.state.report.filters[row].replace("'", "''") + "'");
     }.bind(this));
     return filter_array.join(' AND ');
   }
