36c8e02d6ad1401c9f499e68f4030b14e23f1841
John Dimm <johndimm@johns-air.attlocal.net>
2019-08-25 08:33:40 -0700

    app -> breakdown for easier upload to doteasy.

--- bin/babel.sh
+++ bin/babel.sh
@@ -1,3 +1,3 @@
-npx babel --out-dir ../app/javascript/ $1
+npx babel --out-dir ../breakdown/javascript/ $1
 #/Users/jdimm/workspace/customer-client/node_modules/.bin/babel --presets /Users/jdimm/workspace/customer-client/node_modules/babel/babel-preset-react ./$1 --out-dir ../app/javascript
 
