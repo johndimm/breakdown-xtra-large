69072040b4e1e9b8b7a13e14be7dff7af818a89f
John Dimm <johndimm@johns-air.attlocal.net>
2019-08-29 13:55:11 -0700

    check that the downloaded file contains "transactions" in its name.

--- extension/background.js
+++ extension/background.js
@@ -6,13 +6,13 @@ chrome.downloads.onChanged.addListener(function(delta) {
 
     chrome.downloads.search({}, function(results){
       var filename = results[0].filename;
-      if (filename != '') {
+      if (filename.includes("transactions")) {
         //
         // The user downloaded transactions from Mint.  Time to pop up a new tab
         // for Breakdown.
         //
-        // var url = 'https://dprhcp006.doteasy.com/~johndimm/breakdown/index.html?dataset=' + filename;
-        var url = 'http://localhost/projects/breakdown/breakdown/index.html?dataset=' + filename;
+        var url = 'https://dprhcp006.doteasy.com/~johndimm/breakdown/index.html?dataset=' + filename;
+        // var url = 'http://localhost/projects/breakdown/breakdown/index.html?dataset=' + filename;
         chrome.tabs.create({"url": url});
       }
     });
