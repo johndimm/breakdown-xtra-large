5339ef672e27b29e1d6babdb1ca65378cf837a2a
John Dimm <johndimm@johns-mbp.attlocal.net>
2020-06-24 19:23:33 -0700

    Connection string for mysql.

---
+++ breakdown/php/mysql_connect.php
@@ -0,0 +1,7 @@
+<?php
+    // Installation:  supply the missing credentials and save this file as mysql_connect.php
+    $mysql_host = "127.0.0.1";
+    $mysql_user = "";
+    $mysql_password = "";
+    $mysql_database = "";
+?>
